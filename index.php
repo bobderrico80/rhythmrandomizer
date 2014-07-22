d9f1a2b237b731b861343c0268cc37545caef335
bobderrico80 <bobderrico80@gmail.com>
2014-07-22 01:45:09 -0400

    Caught up to old site
    New site now has basic functionality of old site.  Removed index.html in
    favor of index.php.  Also added new style sheet.

--- index.php
+++ index.php
@@ -19,7 +19,7 @@ try {
     <head>
         <title>The Rhythm Randomizer</title>
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-        <link rel="stylesheet" type="text/css" href="stylesheet-new.css"/>
+        <link rel="stylesheet" type="text/css" href="stylesheet.css"/>
         <meta name="viewport" content="width=device-width"/>
     </head>
     <body>
@@ -37,17 +37,21 @@ try {
         <div id="options">
             <div id="optionTabs">
                 <ul>
-                    <li>General Options</li>
-                    <li>Note Options</li>
+                    <li class="selected" id="generalOptionsTab">General Options</li>
+                    <li id="noteOptionsTab">Note Options</li>
                 </ul>
-                <div id="generalOptions">
-                    <label for="timeSignature">Time Signature:</label>
+            </div>
+            <div id="generalOptions" class="optionsPage">
+                <div class="selectionWrapper">
+                <label for="timeSignature">Time Signature:</label>
                     <select id="timeSignature">
                         <option value="4" selected>4/4 Time</option>
                         <option value="3">3/4 Time</option>
                         <option value="2">2/4 Time</option>
                     </select>
-                    <label for="phraseLength">Phrase Length</label>
+                </div>
+                <div class="selectionWrapper">
+                <label for="phraseLength">Phrase Length:</label>
                     <select id="phraseLength">
                         <option value="1">1 measure</option>
                         <option value="2" selected>2 measures</option>
@@ -55,57 +59,59 @@ try {
                         <option value="8">8 measures</option>
                     </select>
                 </div>
+            </div>
+            <?php
+                //SELECT available note groups
+                $sql = 'SELECT
+                            noteGroups.noteGroupID,
+                            noteGroups.noteGroupName,
+                            noteGroups.noteGroupGraphic,
+                            noteCats.noteCatName,
+                            noteGroups.noteGroupDefault
+                        FROM
+                                noteGroups
+                        INNER JOIN
+                                noteCats
+                        ON
+                                noteGroups.noteGroupNoteCatID = noteCats.noteCatID
+                        ORDER BY
+                                noteCats.noteCatID,
+                                noteGroups.noteGroupID';
+                $stmt = $db->query($sql);
+            ?>
+            <div id="noteOptions" class="optionsPage">
                 <?php
-                    //SELECT available note groups
-                    $sql = 'SELECT
-                                noteGroups.noteGroupID,
-                                noteGroups.noteGroupName,
-                                noteGroups.noteGroupGraphic,
-                                noteCats.noteCatName,
-                                noteGroups.noteGroupDefault
-                            FROM
-                                    noteGroups
-                            INNER JOIN
-                                    noteCats
-                            ON
-                                    noteGroups.noteGroupNoteCatID = noteCats.noteCatID
-                            ORDER BY
-                                    noteCats.noteCatID,
-                                    noteGroups.noteGroupID';
-                    $stmt = $db->query($sql);
-                ?>
-                <div id="noteOptions">
-                    <?php
-                        $lastCat = '';
-                        while ($row = $stmt->fetch()) {
-                            //creates field set for items in the same category
-                            if ($lastCat != $row['noteCatName']) {
-                                if ($lastCat != '') {
-                                    echo '</fieldset>';
-                                }
-                                echo '<fieldset>';
-                                echo '<legend>' . $row['noteCatName'] . '</legend>';
+                    $lastCat = '';
+                    while ($row = $stmt->fetch()) {
+                        //creates field set for items in the same category
+                        if ($lastCat != $row['noteCatName']) {
+                            if ($lastCat != '') {
+                                echo '</fieldset>';
                             }
-                            
-                            //Determines if checkbox should be pre-selected
-                            if ($row['noteGroupDefault'] == 1) {
-                                $checked = 'checked';
-                            } else {
-                                $checked = '';
-                            }
-                            
-                            //creates checkboxes and images for each note group option
-                            echo '<input type="checkbox" id="' . $row['noteGroupID'] . 
-                                    '" title="' . $row['noteGroupName'] . '" value="' . 
-                                    $row['noteGroupID'] . '" '. $checked . '/>';
-                            echo '<img class="noteLabel" src="notes/' . $row['noteGroupGraphic'] . 
-                                    '.png" title="' . $row['noteGroupName'] . '" alt="' . $row['noteGroupName'] . '"/>';
-                            $lastCat = $row['noteCatName'];
-                    
+                            echo '<fieldset>';
+                            echo '<legend>' . $row['noteCatName'] . '</legend>';
                         }
-                        echo '</fieldset>';
-                    ?>
-                </div>
+
+                        //Determines if checkbox should be pre-selected
+                        if ($row['noteGroupDefault'] == 1) {
+                            $checked = 'checked';
+                        } else {
+                            $checked = '';
+                        }
+
+                        //creates checkboxes and images for each note group option
+                        echo '<input type="checkbox" id="' . $row['noteGroupID'] . 
+                                '" title="' . $row['noteGroupName'] . '" value="' . 
+                                $row['noteGroupID'] . '" '. $checked . '/>';
+                        echo '<div class="noteLabelWrapper">';
+                        echo '<img class="noteLabel" src="notes/' . $row['noteGroupGraphic'] . 
+                                '.png" title="' . $row['noteGroupName'] . '" alt="' . $row['noteGroupName'] . '"/>';
+                        echo '</div>';
+                        $lastCat = $row['noteCatName'];
+
+                    }
+                    echo '</fieldset>';
+                ?>
             </div>
         </div>
         <script>
@@ -190,6 +196,22 @@ try {
             //Resize window
             $(window).resize(function(){
                scaleRhythm(); 
+            });
+            
+            //Note Options Tab Click
+            $("#noteOptionsTab").click(function(){
+                $("#noteOptionsTab").addClass("selected");
+                $("#generalOptionsTab").removeClass("selected");
+                $("#noteOptions").show();
+                $("#generalOptions").hide();
+            });
+            
+            //General Options Tab Click
+            $("#generalOptionsTab").click(function(){
+                $("#generalOptionsTab").addClass("selected");
+                $("#noteOptionsTab").removeClass("selected");
+                $("#generalOptions").show();
+                $("#noteOptions").hide();
             });
         </script>
     </body>
