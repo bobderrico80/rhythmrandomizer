3dba8072591eeb5ea7d88028280425d271d5dd58
Bob D'Errico <bob@bobderrico.com>
2014-11-12 20:45:42 -0500

    Revert "Began UI redesign"
    This reverts commit 85ea88a6439bfa4ee901a7e32de52704fb5ac3a1.

--- index.php
+++ index.php
@@ -11,16 +11,25 @@
         <meta name="viewport" content="width=device-width"/>
     </head>
     <body>
-        <div class="banner">
-            <img src="img/hamburger.png" title="Options" class="optionsButton" alt="options"/>
+        <div id="banner">
             <h1>The Rhythm Randomizer</h1>
         </div>
-        <div class="options">
-            <div class="generalOptionsTab optionTabs">
-                <div class="rightArrow"></div>
-                <div class="optionsTabLabel">General Options</div>
+        <div id="rhythmContainer" title="Click the rhythm to generate a new one!">
+            <div class="preloader">
+                <img src="preloader.gif" alt="loading..." title="loading..."/>
             </div>
-            <div class="generalOptions optionsPage">
+            <div id="rhythm">
+
+            </div>
+        </div>
+        <div id="options">
+            <div id="optionTabs">
+                <ul>
+                    <li id="generalOptionsTab">General Options</li>
+                    <li id="noteOptionsTab">Note Options</li>
+                </ul>
+            </div>
+            <div id="generalOptions" class="optionsPage">
                 <div class="selectionWrapper">
                 <label for="timeSignature">Time Signature:</label>
                     <select id="timeSignature">
@@ -38,10 +47,6 @@
                         <option value="8">8 measures</option>
                     </select>
                 </div>
-            </div>
-            <div class="noteOptionsTab optionTabs">
-                <div class="downArrow"></div>
-                <div class="optionsTabLabel">Note Options</div>
             </div>
             <?php
                 //SELECT available note groups
@@ -99,14 +104,6 @@
                 ?>
             </div>
         </div>
-        <div class="rhythmContainer" title="Click the rhythm to generate a new one!">
-            <div class="preloader">
-                <img src="preloader.gif" alt="loading..." title="loading..."/>
-            </div>
-            <div class="rhythm">
-
-            </div>
-        </div>
         <script>
             /***************/
             /** FUNCTIONS **/
@@ -115,7 +112,7 @@
             //Scales the rhythm to fit the width of the screen.
             function scaleRhythm() {
                 //Get width of rhythm at full resolution
-                var rhythmWidth = $(".rhythm").width();
+                var rhythmWidth = $("#rhythm").width();
 
                 //Get current screen/window width
                 var screenWidth = window.innerWidth;
@@ -124,7 +121,7 @@
                 var ratio = screenWidth / rhythmWidth;
 
                 //Get the total number of systems in the rhythm
-                var systemCount = $(".rhythm").find(".system").length;
+                var systemCount = $("#rhythm").find(".system").length;
 
                 //Compute the new note height
                 var newNoteHeight = $(".note").height() * ratio * .9;
@@ -139,15 +136,15 @@
 
                 //Set new note height, and new top margin (to center rhythm in container)
                 $(".note").css("height",newNoteHeight);
-                $(".rhythm").css("margin-top",(300 - $(".rhythm").height()) / 2);
+                $("#rhythm").css("margin-top",(300 - $("#rhythm").height()) / 2);
             }
 
             //spaces notes evenly across multiple systems
             function spaceNotes() {
                 $(".system").each(function(){
                     $(this).find('.spacer').css("width",0);
-                    if ($(this).width() < $(".rhythm").width()) {
-                        var sizeDiff = $(".rhythm").width() - $(this).width();
+                    if ($(this).width() < $("#rhythm").width()) {
+                        var sizeDiff = $("#rhythm").width() - $(this).width();
                         var spacerCount = $(this).find('.spacer').length;
                         var spacerSize = sizeDiff / spacerCount;
                         $(this).find('.spacer').css("width",spacerSize);
@@ -156,35 +153,37 @@
                     }
                 });
             }
+
             //Slides down note options
             function showNoteOptions(){
-            //    $(".noteOptionsTab").addClass("selected");
-                // $(".generalOptionsTab").removeClass("selected");
-                // $(".noteOptions").slideDown();
-                // $(".generalOptions").hide();
+                $("#noteOptionsTab").addClass("selected");
+                $("#generalOptionsTab").removeClass("selected");
+                $("#noteOptions").slideDown();
+                $("#generalOptions").hide();
             }
 
             //Slides down general options
             function showGeneralOptions(){
-                // $(".generalOptionsTab").addClass("selected");
-                // $(".noteOptionsTab").removeClass("selected");
-                // $(".generalOptions").slideDown();
-                // $(".noteOptions").hide();
+                $("#generalOptionsTab").addClass("selected");
+                $("#noteOptionsTab").removeClass("selected");
+                $("#generalOptions").slideDown();
+                $("#noteOptions").hide();
             }
 
             //Slides up options panels
             function clearOptions() {
-                // $(".generalOptionsTab").removeClass("selected");
-                // $(".noteOptionsTab").removeClass("selected");
-                // $(".generalOptions").slideUp();
-                // $(".noteOptions").slideUp();
+                $("#generalOptionsTab").removeClass("selected");
+                $("#noteOptionsTab").removeClass("selected");
+                $("#generalOptions").slideUp();
+                $("#noteOptions").slideUp();
             }
+
             /*********************/
             /** EVENT LISTENERS **/
             /*********************/
 
             //Random Rhythm Click
-            $(".rhythmContainer").click(function(){
+            $("#rhythmContainer").click(function(){
 
                 //slide up options panels
                 clearOptions();
@@ -226,7 +225,7 @@
                     },
                     type : "GET",
                     success : function(response) {
-                        $(".rhythm").html(response);
+                        $("#rhythm").html(response);
                         $(".note").imagesLoaded(function(){
                             scaleRhythm();
                             spaceNotes();
@@ -235,7 +234,7 @@
                         });
                     },
                     error : function(xhr, status, errorThrown) {
-                        $(".rhythm").html("<p>There was a problem getting your rhythm.</p>" +
+                        $("#rhythm").html("<p>There was a problem getting your rhythm.</p>" +
                                 "<p>Sorry about that.  Please click the Randomize " +
                                 "button again.  If you are still getting this error, " +
                                 "please <a href=\"mailto:bob@derricowebdesign.com\">" +
@@ -247,7 +246,7 @@
             });
 
             //Page load
-            $(function(){$(".rhythmContainer").click();});
+            $(function(){$("#rhythmContainer").click();});
 
             //Resize window
             var resizeTimer;
@@ -264,8 +263,8 @@
             });
 
             //Note Options Tab Click
-            $(".noteOptionsTab").click(function(){
-                if ($(".noteOptions").is(":visible")) {
+            $("#noteOptionsTab").click(function(){
+                if ($("#noteOptions").is(":visible")) {
                     clearOptions();
                 } else {
                     showNoteOptions();
@@ -273,8 +272,8 @@
             });
 
             //General Options Tab Click
-            $(".generalOptionsTab").click(function(){
-                if ($(".generalOptions").is(":visible")) {
+            $("#generalOptionsTab").click(function(){
+                if ($("#generalOptions").is(":visible")) {
                     clearOptions();
                 } else {
                     showGeneralOptions();
